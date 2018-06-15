b51dce726eb9d9b958cdfde9f37e0af16a39101f
RobFiola <38474642+RobFiola@users.noreply.github.com>
2018-06-15 15:25:34 -0500

    4

--- java.js
+++ java.js
@@ -1,12 +1,13 @@
 //var href = window.location.href;
 //console.log(href);
+var xmlhttp=null;
 var product=getQueryVariable('product');
 console.log('product: '+product);
 var asin=getQueryVariable('asin');
-console.log('asin: '+product);
+console.log('asin: '+asin);
 var country=getQueryVariable('country');
-console.log('country: '+product);
-
+console.log('country: '+country);
+openJSON();
 
 var link = "https://www.amazon.com";
 alert('redirect to :'+link);
@@ -16,13 +17,23 @@ function getQueryVariable(variable)//country
 {
     
 var query=window.location.search.substring(1);
-    console.log(query);
     var vars=query.split('&');
-    console.log('vars: '+vars);
     for (var i=0;i<vars.length;i++){
         var pair=vars[i].split('=');
-        console.log(pair);
         if(pair[0]==variable){return pair[1];}
     }
     return false;
+}
+
+function openJSON(){
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = JSON.parse(this.responseText);
+            document.getElementById('super').value=myObj.armband98d.super;
+            
+        }
+    };
+    xmlhttp.open("GET", "redirects.json", true);
+    xmlhttp.send();
 }
