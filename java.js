d8c8645fac211659c3ed0ce1337975b093706467
nateamz <38504615+nateamz@users.noreply.github.com>
2018-06-15 14:05:21 -0500

    1

--- java.js
+++ java.js
@@ -1,2 +1,25 @@
+var href = window.location.href;
+var pieces = href.split('?');
+var product='';
+var asin='';
+var country='';
+
+for (var i=1;i<pieces.length;i++)
+{
+    var variable=pieces[i].split('=');
+    if (variable[0]=='product') {
+        product = variable[1];
+    }
+    elseif (variable[0]=='asin'){
+        asin=variable[1]
+    }
+    elseif (variable[0]=='country'){
+        country=variable[1];
+    }
+    console.log('product: '+product+'/n'
+               +'asin: '+ asin +'/n'
+               +'country: '+country);
+}
 var link = "https://www.amazon.com";
-window.open(link,"_self");
+alert('redirect to :'+link);
+//window.open(link,"_self");
